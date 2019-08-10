90cc0d60f8fb1de34b48482ef64f7b815457cd88
Aleksandr Panasenko <aleksandr.panasenko@netcracker.com>
2019-08-10 22:39:21 +0300

    VK component

--- src/app/App.js
+++ src/app/App.js
@@ -1,25 +1,40 @@
 import React from "react";
+import {View, Panel, PanelHeader, Group, List, Cell, Div, Button, Gallery, Avatar, Counter} from "@vkontakte/vkui";
+
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <svg className='App-logo'>
-                    <use xlinkHref='#icon-logo' />
-                </svg>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <View activePanel="main">
+            <Panel id="main">
+                <PanelHeader>VK App</PanelHeader>
+                <Group title="Запись">
+                    <Div className="header">
+                        <Cell
+                            size="l"
+                            description="Рен-ТВ"
+                            before={<Avatar src="https://pp.userapi.com/SslEXxXXambnM5qNlF_WTh6S_Y0fVDlKiDvsiQ/fd0PgTJ9Xfk.jpg"/>}
+                        >
+                            Апостол Пётр
+                        </Cell>
+                        <Button className={"activeSubs"} level="commerce" size="l" after={<Counter>4</Counter>}>Активных подписок</Button>
+                    </Div>
+
+                    <Gallery
+                        slideWidth="90%"
+                        style={{ height: 150 }}
+                        bullets="dark"
+                    >
+                        <div style={{ backgroundColor: 'var(--destructive)' }} />
+                        <div style={{ backgroundColor: 'var(--button_commerce_background)' }} />
+                        <div style={{ backgroundColor: 'var(--accent)' }} />
+                    </Gallery>
+                    <Div className={"footer"}>
+                        <Button level="commerce">Подписаться за 299&#8381;/месяц</Button>
+                        <Button level="destructive">Пожаловаться</Button>
+                    </Div>
+                </Group>
+            </Panel>
+        </View>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ const __svg__           = { path: "./assets/icons/*.svg", name: "assets/icons/[h
 require("webpack-svgstore-plugin/src/helpers/svgxhr")(__svg__);
 import React from "react";
 import ReactDOM from "react-dom";
+import "@vkontakte/vkui/dist/vkui.css";
 import "./assets/styles/base.less";
+
 import App from "./app/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
